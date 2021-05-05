7026738a22d5e49aab0a5f4f98130b57c08abf8e
rakkeshasa <rakkeshasa2@gmail.com>
2021-05-05 22:00:48 +0900

    FBV로 블로그 포스트 상세 페이지 만들기

--- blog/models.py
+++ blog/models.py
@@ -5,11 +5,14 @@ class Post(models.Model):
     content = models.TextField()
 
     created_at = models.DateTimeField()
-    #author: 추후 작성 예정정
+    #author: 추후 작성 예정
 
     created_at = models.DateTimeField(auto_now_add=True)
     created_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return f'[{self.pk}]{self.title}'
+
+    def get_absolute_url(self):
+        return f'/blog/{self.pk}/'
 # reate your models here.
